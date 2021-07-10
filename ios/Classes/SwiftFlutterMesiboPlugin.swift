07e02346e5b4c4c6741626ab6b4e5c41003b585e
Felix Jordan <felix.jordan@buddy-app.de>
2021-07-10 13:53:01 +0200

    feat(iOS): auto start/stop Mesibo when application did enter bacground or become active

--- ios/Classes/SwiftFlutterMesiboPlugin.swift
+++ ios/Classes/SwiftFlutterMesiboPlugin.swift
@@ -10,14 +10,22 @@ extension OSLog {
     static let mesiboIosBinding = OSLog(subsystem: subsystem, category: "mesiboIosBinding")
 }
 
-public class SwiftFlutterMesiboPlugin: NSObject, FlutterPlugin {
+public class SwiftFlutterMesiboPlugin: NSObject, FlutterPlugin, FlutterApplicationLifeCycleDelegate {
+    
+    public static let staticInstance = SwiftFlutterMesiboPlugin();
     
     let modelMapper: BindingModelMapper = BindingModelMapper.init()
     var delegatingMesiboListener: DelegatingMesiboListener!
     var realTimeApiImpl: IosMesiboRealTimeApi!
     
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let instance = SwiftFlutterMesiboPlugin()
+        let instance = SwiftFlutterMesiboPlugin.staticInstance
+        
+        // listen for life cycle changes
+        registrar.addApplicationDelegate(instance)
+        os_log("registered", log: OSLog.mesiboIosBinding, type: .info)
+        
+        // setup Mesibo
         instance.setupMesiboPlatformBinding(registrar: registrar)
     }
     
@@ -38,5 +46,31 @@ public class SwiftFlutterMesiboPlugin: NSObject, FlutterPlugin {
         // General Mesibo configuration
         // TODO maybe refactor into own initMesibo method as we do on Android
         Mesibo.getInstance()!.setSecureConnection(true)
+    }
+    
+    public func applicationDidBecomeActive(_ application: UIApplication) {
+        os_log("applicationDidBecomeActive", log: OSLog.mesiboIosBinding, type: .info)
+    }
+    
+    public func applicationWillTerminate(_ application: UIApplication) {
+        os_log("applicationWillTerminate", log: OSLog.mesiboIosBinding, type: .info)
+        
+        Mesibo.getInstance()!.stop()
+    }
+    
+    public func applicationWillResignActive(_ application: UIApplication) {
+        os_log("applicationWillResignActive", log: OSLog.mesiboIosBinding, type: .info)
+    }
+    
+    public func applicationDidEnterBackground(_ application: UIApplication) {
+        os_log("applicationDidEnterBackground", log: OSLog.mesiboIosBinding, type: .info)
+        
+        Mesibo.getInstance()!.stop()
+    }
+    
+    public func applicationWillEnterForeground(_ application: UIApplication) {
+        os_log("applicationWillEnterForeground", log: OSLog.mesiboIosBinding, type: .info)
+        
+        Mesibo.getInstance()!.start()
     }
 }
